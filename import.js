8b0998b7c4b1787e4b9a34512d84e91c27f3d4cb
RajabDildar <rajabdildar4678@gmail.com>
2025-01-11 19:55:39 +0500

    random-words package

--- import.js
+++ import.js
@@ -1,4 +1,8 @@
 import { sum, PI } from "./math2.js";
+import { generate } from "random-words";
+
+console.log(generate());
+console.log(generate(5));
 console.log(sum(1, 2));
 console.log(PI);
 //to use import, we have to use "type" : "module" in package.json
